9cc4d43e5ecee80b9b1a8debd580fd694a9b8454
Breno Zukowski <brenozukotech@gmail.com>
2022-12-20 19:37:41 -0300

    Created global styles, added services and new lib registry

--- pages/test/jobs/index.tsx
+++ pages/test/jobs/index.tsx
@@ -1,3 +1,51 @@
-export default function Jobs() {
-  return <></>;
+import { GetServerSideProps } from "next";
+import { AxiosResponse } from "axios";
+
+// STYLES
+import * as S from "../../../styles/jobs";
+
+//SERVICES
+import { getJobs, IRequestPayload } from "../../../services/jobs";
+
+interface IJobItem {
+  jobId: string;
+  jobTitle: string;
+  companyName: string;
+  jobDescription: string;
 }
+
+interface IJobsList {
+  list: IJobItem[];
+}
+
+export default function Jobs({ list }: IJobsList) {
+  console.log("the list is ", list);
+  return (
+    <>
+      <div>Teste </div>
+      <S.ContainerTest>teste</S.ContainerTest>
+      {list.map((job) => <div key={job.jobId}>{job.jobTitle}</div>)}
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const requestParameters: IRequestPayload = {
+    companySkills: true,
+    dismissedListingHashes: [],
+    fetchJobDesc: true,
+    jobTitle: "Business Analyst",
+    locations: [],
+    numJobs: 10,
+    previousListingHashes: [],
+  };
+
+  const jobsResponse = await getJobs(requestParameters);
+  const jobsData: IJobItem[] = await jobsResponse!.data.jobs;
+
+  return {
+    props: {
+      list: jobsData,
+    }, // will be passed to the page component as props
+  };
+};
--- styles/jobs.ts
+++ styles/jobs.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
+export const ContainerTest = styled.div`
+  border-radius: 10px;
 
+  &:after {
+    content: "";
+    width: 300px;
+    background-color: red;
+    height: 300px;
+  }
+`;
