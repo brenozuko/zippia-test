06eb40e63571656d809db7b5458d3dd19c857415
Breno Zukowski <brenozukotech@gmail.com>
2022-12-20 20:18:34 -0300

    Tested styles

--- styles/jobs.ts
+++ styles/jobs.ts
@@ -1,12 +1,5 @@
 import styled from "styled-components";
 
 export const ContainerTest = styled.div`
-  border-radius: 10px;
-
-  &:after {
-    content: "";
-    width: 300px;
-    background-color: red;
-    height: 300px;
-  }
+  color: red;
 `;
